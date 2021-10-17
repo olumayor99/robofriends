e27ce264afbf0f30448682a5e0bd52d129352112
Olumayowa Taiwo <olumayoor99@gmail.com>
2021-10-17 17:51:32 +0300

    cleaned the initial template and files, created card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+const Card = () => {
+    return (
+    <div>
+        <img alt='robots' src='' />
+        <div>
+            <h2>Jane Doe</h2>
+            <p>jane.doe@gmail.com</p>
+        </div>
+    
+    </div>
+    )
+}
+
+export default Card;
